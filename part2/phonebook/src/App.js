31407cc370e7a35b8c94ad5cf47155d88aaa8af4
Kai Shi <shi.kai@outlook.com>
2022-03-10 01:15:37 -0500

    finish exercises 2.19-2.29

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,9 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import SuccessNotification from "./components/SuccessNotification";
+import ErrorNotification from "./components/ErrorNotification";
 import personService from "./services/personService";
 
 const App = () => {
@@ -10,10 +11,24 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [keyword, setKeyword] = useState("");
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => setPersons(initialPersons));
   }, []);
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const hanldeKeywordChange = (event) => {
+    setKeyword(event.target.value);
+  };
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -30,6 +45,10 @@ const App = () => {
 
       personService.create(personObject).then((returnPerson) => {
         setPersons(persons.concat(returnPerson));
+        setSuccessMessage(`Added ${returnPerson.name}`);
+        setTimeout(() => {
+          setSuccessMessage(null);
+        }, 2000);
         setNewName("");
         setNewNumber("");
       });
@@ -41,32 +60,39 @@ const App = () => {
       if (isConfirm) {
         const changedPerson = { ...person, number: newNumber };
 
-        personService.update(person.id, changedPerson).then((returnPerson) => {
-          setPersons(
-            persons.map((item) => (item.id === person.id ? returnPerson : item))
-          );
-        });
+        personService
+          .update(person.id, changedPerson)
+          .then((returnPerson) => {
+            setPersons(
+              persons.map((item) =>
+                item.id === person.id ? returnPerson : item
+              )
+            );
+            setSuccessMessage(`Updated ${returnPerson.name} number`);
+            setTimeout(() => {
+              setSuccessMessage(null);
+            }, 2000);
+            setNewName("");
+            setNewNumber("");
+          })
+          .catch((error) => {
+            setErrorMessage(
+              `Information of ${person.name} was already removed from server`
+            );
+            setTimeout(() => {
+              setErrorMessage(null);
+            }, 2000);
+            setPersons(persons.filter(({ id }) => id !== person.id));
+          });
       }
     }
-  };
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
-
-  const hanldeKeywordChange = (event) => {
-    setKeyword(event.target.value);
   };
 
   const deletePerson = (person) => {
     const isConfirm = window.confirm(`Delete ${person.name}?`);
 
     if (isConfirm) {
-      personService.remove().then((response) => {
+      personService.remove(person.id).then((response) => {
         setPersons(persons.filter(({ id }) => id !== person.id));
       });
     }
@@ -75,6 +101,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <SuccessNotification message={successMessage} />
+      <ErrorNotification message={errorMessage} />
       <Filter keyword={keyword} hanldeKeywordChange={hanldeKeywordChange} />
       <h3>add a new</h3>
       <PersonForm
